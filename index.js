0b726e75ff5c84053ceff5605ad529a28666a6d6
KhoirulHS <wibuadalahrasterkuat@gmail.com>
2025-11-25 15:17:49 +0700

    fix: update new image

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ console.log("External JS berhasil dijalankan!ss");
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.getElementById('navbars');
     if (!navbar) return;
-
+    // 1rem=32p
     window.addEventListener('scroll', () => {
-        if (window.scrollY > window.innerHeight*0.20) {
+        if (window.scrollY > window.innerHeight*1 + 64) {
             navbar.classList.replace('text-white', 'text-black');
+            console.log(scrollY)
         } else {
             navbar.classList.replace('text-black', 'text-white');
         }
     });
 });
+
+
 
 document.getElementById('menu-toggle').addEventListener('click', () => {
     document.getElementById('mobile-menu').classList.toggle('hidden');
