81b3f376346085da4739968d69d73f18d1972e8b
KhoirulHS <wibuadalahrasterkuat@gmail.com>
2025-11-27 16:40:41 +0700

    fix: css

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ console.log("External JS berhasil dijalankan!ss");
 const facebookNav = document.getElementById("facebook_nav");
 const twitterNav = document.getElementById("twitter_nav");
 const discordNav = document.getElementById("discord_nav");
+const reachMe = document.getElementById("reach-me");
 
 document.addEventListener('DOMContentLoaded', () => {
     const navbar = document.getElementById('navbars');
     if (!navbar) return;
     // 1rem=32p
     window.addEventListener('scroll', () => {
-        if (window.scrollY > window.innerHeight*1 + 64) {
+        if (window.scrollY > window.innerHeight*1 + (64*2)) {
             facebookNav.src = "ff--facebook.png";
             twitterNav.src = "ff--twitter.png";
             discordNav.src = "ff--discord.png";
